10015a753645c4419489b3ff13171e87fa32f26b
Pana Florin Ionut <panaflorinionut@gmail.com>
2024-03-16 22:31:37 +0200

    Update CV

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-// Add any Javascript functionality here
-// This example uses Javascript to add a click event listener to the navigation link
-// You can expand on this to add more interactivity
-
-const navLink = document.querySelector(".nav a");
-
-// navLink.addEventListener('click', function(event)) {
-//   event.preventDefault();
-//   const targetSection = document.getElementById(this.getAttribute('href').slice(1));
-//   targetSection.scroll}
-const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".nav");
-
 const accordionItems = document.querySelectorAll(".accordion-item");
 
 accordionItems.forEach((item) => {
   const accordionButton = item.querySelector(".accordion-button");
   accordionButton.addEventListener("click", () => {
+    const currentActiveItem = document.querySelector(".accordion-item.active");
+
+    // If another item is already open, close it
+    if (currentActiveItem && currentActiveItem !== item) {
+      currentActiveItem.classList.remove("active");
+    }
+
     item.classList.toggle("active");
   });
 });
