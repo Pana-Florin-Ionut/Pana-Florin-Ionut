9dd97c62c254ef5da194cf763533fd376b33687f
Pana Florin Ionut <panaflorinionut@gmail.com>
2024-06-07 23:40:40 +0300

    New files

---
+++ carouselScript.js
@@ -0,0 +1,27 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll(".carousel-images img");
+  const totalSlides = slides.length;
+
+  if (index >= totalSlides) {
+    currentIndex = 0;
+  } else if (index < 0) {
+    currentIndex = totalSlides - 1;
+  } else {
+    currentIndex = index;
+  }
+
+  const offset = -currentIndex * 100;
+  document.querySelector(
+    ".carousel-images"
+  ).style.transform = `translateX(${offset}%)`;
+}
+
+function moveSlide(step) {
+  showSlide(currentIndex + step);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showSlide(currentIndex);
+});
